b7f36117b2b1d52ac6def060a1decbbc2141080a
Johnathan Aspinwall <jwaspin@gmail.com>
2024-10-22 17:52:09 -0600

    feat: implement dynamic bar color logic in Provider component based on task average TAT

--- src/components/task-item/provider/provider.tsx
+++ src/components/task-item/provider/provider.tsx
@@ -2,10 +2,30 @@ import React from "react";
 import { TaskItemProps } from "../task-item";
 import styles from "./provider.module.css";
 
-export const Provider: React.FC<TaskItemProps> = ({ task, isSelected }) => {
-  const barColor = isSelected
-    ? task.styles.backgroundSelectedColor
-    : task.styles.backgroundColor;
+const greenBarColor = "#27a38a";
+const yellowBarColor = "#d1a215";
+const redBarColor = "#bf0808";
+const barBackgroundColor = "#b8c2cc";
+
+const good_tat_threshold = 1.5;
+const warning_tat_threshold = 2.0;
+
+const getBarColor = (task: TaskItemProps["task"]) => {
+  if (task.tat_avg) {
+    if (task.tat_avg < good_tat_threshold) {
+      return greenBarColor;
+    } else if (task.tat_avg < warning_tat_threshold) {
+      return yellowBarColor;
+    }
+    return redBarColor;
+  }
+  return barBackgroundColor;
+};
+
+export const Provider: React.FC<TaskItemProps> = ({
+  task /* , isSelected */,
+}) => {
+  const barColor = getBarColor(task);
 
   const providerWith = task.x2 - task.x1;
 
@@ -17,16 +37,6 @@ export const Provider: React.FC<TaskItemProps> = ({ task, isSelected }) => {
         width={providerWith}
         y={task.y}
         height={task.height}
-        rx={task.barCornerRadius}
-        ry={task.barCornerRadius}
-        className={styles.providerBackground}
-      />
-      <rect
-        fill={barColor}
-        x={task.x1}
-        width={providerWith}
-        y={task.y}
-        height={task.height / 2}
         rx={task.barCornerRadius}
         ry={task.barCornerRadius}
         className={styles.providerTop}
